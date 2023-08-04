14710c3a05fe9d0700108c49c908514e14e00a34
zigBalthazar <zigbalthazar@gmail.com>
2023-08-04 18:42:34 +0330

    feat: operations

--- src/set.go
+++ src/set.go
@@ -19,3 +19,74 @@ func (s *Sets) NewSet(key string) {
 func (s *Set) NewElement(key string, v Value) {
 	s.Elements.AddElement(key, v)
 }
+
+// Get Sets Name
+func (s *Sets) GetSetsName() []string {
+	keys := make([]string, 0, len(*s))
+	for key := range *s {
+		keys = append(keys, key)
+	}
+	return keys
+}
+
+// Drop Set
+func (s *Sets) DropSet(SetName string) {
+	delete(*s, SetName)
+}
+
+// Clean Set
+func (s *Sets) CleanSet(SetName string) {
+	if _, ok := (*s)[SetName]; ok {
+		(*s)[SetName] = Set{}
+	}
+}
+
+// Get Elements of a set
+func (s *Set) Get(Key string) Value {
+	return s.Elements[Key]
+}
+
+// Count elements of a set
+func (s *Set) Count() int {
+	return len((*s).Elements)
+}
+
+// TODO: Find List of Elements from a set
+
+// Remove Element from a set
+func (s *Set) Remove(Key string) {
+	delete((*s).Elements, Key)
+}
+
+// TODO: Remove list of elements from a set
+
+// Update Element from a set
+func (s *Set) Upadte(Key string, NewValue Value) {
+	(s.Elements)[Key] = NewValue
+}
+
+// TODO: Update list of elements from a set
+
+// Get All Keys
+func (s *Set) GetKeys() []string {
+	keys := make([]string, 0, len(s.Elements))
+	for key := range s.Elements {
+		keys = append(keys, key)
+	}
+	return keys
+}
+
+// Get All Values
+func (s *Set) GetValues() []Value {
+	values := make([]Value, 0, len(s.Elements))
+
+	for v := range s.Elements {
+		values = append(values, s.Elements[v])
+	}
+	return values
+}
+
+// TODO: Create Snapshot
+// TODO: End Snapshot
+
+// TODO: Sort With Limit And Offset
