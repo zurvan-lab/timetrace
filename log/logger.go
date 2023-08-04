2fe848b00357fd9a56edba636c1306cb315feec9
Kay <kehiiiiya@gmail.com>
2023-08-04 17:14:56 +0800

    feat: add logger (#5)

---
+++ log/logger.go
@@ -0,0 +1,59 @@
+package log
+
+import (
+	"encoding/hex"
+	"fmt"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func addFields(event *zerolog.Event, keyvals ...interface{}) *zerolog.Event {
+	if len(keyvals)%2 != 0 {
+		keyvals = append(keyvals, "!MISSING-VALUE!")
+	}
+	for i := 0; i < len(keyvals); i += 2 {
+		key, ok := keyvals[i].(string)
+		if !ok {
+			key = "!INVALID-KEY!"
+		}
+		///
+		switch v := keyvals[i+1].(type) {
+		case fmt.Stringer:
+			event.Stringer(key, v)
+		case []byte:
+			event.Str(key, fmt.Sprintf("%v", hex.EncodeToString(v)))
+		default:
+			event.Any(key, v)
+		}
+	}
+	return event
+}
+
+func Trace(msg string, keyvals ...interface{}) {
+	addFields(log.Trace(), keyvals...).Msg(msg)
+}
+
+func Debug(msg string, keyvals ...interface{}) {
+	addFields(log.Debug(), keyvals...).Msg(msg)
+}
+
+func Info(msg string, keyvals ...interface{}) {
+	addFields(log.Info(), keyvals...).Msg(msg)
+}
+
+func Warn(msg string, keyvals ...interface{}) {
+	addFields(log.Warn(), keyvals...).Msg(msg)
+}
+
+func Error(msg string, keyvals ...interface{}) {
+	addFields(log.Error(), keyvals...).Msg(msg)
+}
+
+func Fatal(msg string, keyvals ...interface{}) {
+	addFields(log.Fatal(), keyvals...).Msg(msg)
+}
+
+func Panic(msg string, keyvals ...interface{}) {
+	addFields(log.Panic(), keyvals...).Msg(msg)
+}
