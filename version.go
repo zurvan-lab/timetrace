7892a843d4e657d8f9ece23f0e51693f8679f55f
MostPow3rful <itskianshahi@gmail.com>
2023-09-03 16:50:51 +0330

    version.go Created [issue #19]

---
+++ version.go
@@ -0,0 +1,49 @@
+package timetracedb
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+)
+
+type Version struct {
+	Meta  string `json:"meta" xml:"meta"`
+	Major uint8  `json:"major" xml:"major"`
+	Minor uint8  `json:"minor" xml:"minor"`
+	Patch uint8  `json:"patch" xml:"patch"`
+}
+
+var version = Version{
+	Meta:  "beta",
+	Major: 0,
+	Minor: 1,
+	Patch: 0,
+}
+
+func Agent() string {
+	return fmt.Sprintf("timetrace/%s", GetVersionAsString())
+}
+
+func GetVersionAsString() string {
+	return fmt.Sprintf("%d.%d.%d-%s", version.Major, version.Minor, version.Patch, version.Meta)
+}
+
+func GetVersionAsJson() (string, error) {
+	v, err := json.Marshal(version)
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(v), nil
+}
+
+func GetVersionAsXML() (string, error) {
+	v, err := xml.MarshalIndent(version, "", " ")
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(v), nil
+}
