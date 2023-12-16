bfc8d97ce092f7fed695407cf51beb0cabbb1478
Kay <kehiiiiya@gmail.com>
2023-12-16 17:06:20 +0000

    Update utils/errors/errors.go

--- utils/errors/errors.go
+++ utils/errors/errors.go
@@ -3,7 +3,7 @@ package errors
 import "errors"
 
 var (
-	// config
+	// config.
 	ErrInavlidConfigPath               = errors.New("invalid config path")
 	ErrInvalidUsers                    = errors.New("invalid user(s)")
 	ErrSpecificAndAllCommandSameAtTime = errors.New("can't have all cmds and specific cmd at same time")
